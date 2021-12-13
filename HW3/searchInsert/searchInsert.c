4449daecfa41fee68a0812ac9649a6124e6e8dc1
geniuseric <e24056386@mail.ncku.edu.tw>
2021-12-13 23:30:11 +0800

    fix: searchInsert.c

--- HW3/searchInsert/searchInsert.c
+++ HW3/searchInsert/searchInsert.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int searchInsert(int* nums, int numsSize, int target){
-    int down = 0, up = numsSize - 1, mid = 0;
+volatile int searchInsert(volatile int* nums, volatile int numsSize, volatile int target){
+    volatile int down = 0, up = numsSize - 1, mid = 0;
     while (down <= up){
-        mid = (down + up) / 2;
+        mid = (down + up) >> 1;
         if (target == nums[mid]) return mid;
         else if (target < nums[mid]) up = mid - 1;
         else down = mid + 1;
@@ -11,10 +11,10 @@ int searchInsert(int* nums, int numsSize, int target){
     return down;
 }
 
-int main(void) {
-    int data[] = {2, 3, 8, 11, 13, 19};
-    int size = 6, target = 13;
-    int index = searchInsert(data, size, target);
+volatile int main(void) {
+    volatile int data[] = {2, 3, 8, 11, 13, 19};
+    volatile int size = 6, target = 13;
+    volatile int index = searchInsert(data, size, target);
     printf("The insert position is %d\n", index);
     return 0;
 }
